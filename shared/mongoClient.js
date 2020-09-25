caf7c038d0464a5b5deff16a9b2c16ade5f2a992
Uriel dos Santos Souza <uriel29@gmail.com>
2020-09-25 12:00:29 -0300

    Add files via upload

---
+++ shared/mongoClient.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+const config = {
+  url: 'mongodb+srv://god:cluster.mongodb.net/',
+};
+
+module.exports = () => new Promise((resolve, reject) => {
+  MongoClient
+    .connect(config.url, { useNewUrlParser: true }, (err, mongoConnection) =>
+      err
+      ? reject(err)
+      : resolve({
+          client: mongoConnection.db(config.dbName),
+          closeConnectionFn: () => setTimeout(() => {
+            mongoConnection.close();
+          }, 1000),
+          mongoConnection,
+        })
+    );
+});
